bbf5d5016ef9d340ce0e2f986d5b43c817c90a9d
Erick Rodriguez <erickrod54@gmail.com>
2021-09-27 01:39:41 -0400

    graphql implementation on ecommerce app

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
@@ -16,9 +15,6 @@ const CartIcon = ({ toggleCartHidden, itemCount }) => (
   </div>
 );
 
-const mapDispatchToProps = dispatch => ({
-  toggleCartHidden: () => dispatch(toggleCartHidden())
-});
 
 const mapStateToProps = createStructuredSelector({
   itemCount: selectCartItemsCount
@@ -26,5 +22,4 @@ const mapStateToProps = createStructuredSelector({
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps
 )(CartIcon);
